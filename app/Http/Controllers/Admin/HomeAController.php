adefb1c7f5eddfbdff60abea74d3aa747a94d558
Gustavo Santos <gustav0.0ggs@gmail.com>
2022-06-23 11:35:09 -0300

    feito toda parte de Dashboard

--- app/Http/Controllers/Admin/HomeAController.php
+++ app/Http/Controllers/Admin/HomeAController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 class HomeAController extends Controller
@@ -13,8 +15,48 @@ class HomeAController extends Controller
 }
 
 
- public function index(){
-    return view('admin.homeA');
+ public function index(Request $request ){
+    $interval = intval($request->input('interval', 30));
+    if($interval > 120){
+        $interval = 120;
+    }
+
+    //contagem de visitantes
+    $dateInterval = date('Y-m-d H:i:s', strtotime('-'.$interval. 'days'));
+    $visitsCount = Visitor::where('date_access', '>=' , $dateInterval)->count();
+
+    //contagem de usuarios online
+    $dateLimit = date('Y-m-d H:i:s' , strtotime('-5 minutes'));
+    $onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)
+    ->groupBy('ip')
+    ->get();
+    $onlineCount = count($onlineList);
+
+    //contagem de páginas
+    $pageCount = Page::count();
+
+    //contagem de usuários
+    $userCount = Page::count();
+
+    //contagem para o pagePie
+
+    $pagePie = [];
+    $visitsAll = Visitor::selectRaw ('page , count(page) as c')->where('date_access', '>=', $dateLimit)->groupBy('page')->get();
+    foreach($visitsAll as $visit){
+        $pagePie[$visit['page'] ] = intval($visit['c']);
+    }
+    $pageLabels = json_encode(array_keys($pagePie));
+    $pageValue = json_encode(array_values($pagePie));
+
+    return view('admin.homeA',[
+        'visitsCount' => $visitsCount,
+        'onlineCount' => $onlineCount,
+        'pageCount' => $pageCount,
+        'userCount' => $userCount,
+        'pageValue' => $pageValue,
+        'pageLabels' => $pageLabels,
+        'dateInterval' => $interval
+    ]);
  }
 
 }
